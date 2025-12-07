0d2e845128fa8510f9b0e7fd72ee21f12ffce7e9
HYY <dideo233@naver.com>
2025-12-08 08:01:37 +0900

    add 10998 A*B

---
+++ src/baekjoon/lv1/Problem10998.java
@@ -0,0 +1,22 @@
+package baekjoon.lv1;
+
+import java.util.Scanner;
+
+/**
+ * 백준 단계별 문제풀이 10998 A+B
+ * 문제: 1x2 = 2, 3x4 = 12 출력
+ * 풀이: Scanner 사용
+ */
+public class Problem10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int result = num1 * num2;
+        System.out.println(result);
+    }
+
+}
+
