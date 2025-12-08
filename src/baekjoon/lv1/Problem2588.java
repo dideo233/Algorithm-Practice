d983ff18a8a25eb5bfebfbdb8954ab9b53c62671
HYY <dideo233@naver.com>
2025-12-09 04:30:00 +0900

    add 2588 자릿수연산

---
+++ src/baekjoon/lv1/Problem2588.java
@@ -0,0 +1,22 @@
+package baekjoon.lv1;
+
+import java.util.Scanner;
+
+public class Problem2588 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+
+        int one = n*(m%10);
+        int ten = n*((m%100)/10);
+        int hundred = n*(m/100);
+
+        int result = one + (ten*10) + (hundred*100);
+
+        System.out.println(one);
+        System.out.println(ten);
+        System.out.println(hundred);
+        System.out.println(result);
+    }
+}
