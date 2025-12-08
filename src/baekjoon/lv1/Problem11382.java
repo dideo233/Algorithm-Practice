caf56edd7ea2f9a5612b39103e1c0fc04f908855
HYY <dideo233@naver.com>
2025-12-09 04:50:31 +0900

    add 11382 long 덧셈연산

---
+++ src/baekjoon/lv1/Problem11382.java
@@ -0,0 +1,16 @@
+package baekjoon.lv1;
+
+import java.util.Scanner;
+
+public class Problem11382 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long a=sc.nextLong();
+        long b=sc.nextLong();
+        long c=sc.nextLong();
+        long sum=a+b+c;
+
+        System.out.println(sum);
+        sc.close();
+    }
+}
