c84e71547b18944ccadecf059f24e5248b3e003f
dideo233 <eltmvpdj11@gmail.com>
2025-12-12 12:41:38 +0900

    add: 접근제한자와 캡슐화 개념 복습 (잔여 push)

---
+++ src/reviewing/step2/sec05/accessmodifier/a/AccessInnerEx.java
@@ -0,0 +1,18 @@
+package reviewing.step2.sec05.accessmodifier.a;
+
+public class AccessInnerEx {
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+
+        // 1. public: 가능
+        data.publicField = 1;
+
+        // 2. default: 가능 (AccessData와 AccessInnerEx는 같은 'a' 패키지)
+        data.defaultField = 2;
+
+        // 3. private: 불가능 (컴파일 에러)
+        // data.privateField = 3;
+
+        data.innerAccess(); // 메서드를 통한 내부 접근은 가능
+    }
+}
---
+++ src/reviewing/step2/sec05/accessmodifier/ex/MaxCounter.java
@@ -0,0 +1,26 @@
+package reviewing.step2.sec05.accessmodifier.ex;
+
+public class MaxCounter {
+    private int count = 0;
+    private int max;
+
+    // 생성자를 통해 최대값을 지정 (외부에서 설정 주입)
+    public MaxCounter(int max) {
+        this.max = max;
+    }
+
+    public void increase() {
+        // 검증 로직. 카운트가 최대값 이상이면 증가시키지 않고 종료
+        if (count >= max) {
+            System.out.println("최대값 " + max + "을 초과할 수 없습니다.");
+            return;
+        }
+        // 실행 로직
+        count++;
+        System.out.println("카운트: " + count);
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
