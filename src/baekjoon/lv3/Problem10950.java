10e2111854d1eb4ab034cdc1114cc64a7b28101e
dideo233 <eltmvpdj11@gmail.com>
2025-12-16 23:32:20 +0900

    add: Problem10950 A+B - 3

---
+++ src/baekjoon/lv3/Problem10950.java
@@ -0,0 +1,22 @@
+package baekjoon.lv3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            String[] str = br.readLine().split(" ");
+
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+            System.out.println(a + b);
+        }
+
+        br.close();
+    }
+}
