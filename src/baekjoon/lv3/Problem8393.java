1585bdd0dfa564b65361a2df7519be3bc26421a8
dideo233 <eltmvpdj11@gmail.com>
2025-12-16 23:35:31 +0900

    add: Problem8393 합

---
+++ src/baekjoon/lv3/Problem8393.java
@@ -0,0 +1,20 @@
+package baekjoon.lv3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem8393 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int total = 0;
+        for(int i = 1; i <= n; i++){
+            total += i;
+        }
+
+        System.out.println(total);
+        br.close();
+    }
+}
