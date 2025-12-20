c48f15f350f8508fdff46ea2d0920aa486a66ad5
dideo233 <eltmvpdj11@gmail.com>
2025-12-20 23:54:17 +0900

    add: Problem2438 별 찍기 - 1

---
+++ src/baekjoon/lv3/Problem2438.java
@@ -0,0 +1,21 @@
+package baekjoon.lv3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= count; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        br.close();
+    }
+}
