ac7e8739faa5b1d0468632de270e8fcb2e5ca012
dideo233 <eltmvpdj11@gmail.com>
2025-12-21 00:15:14 +0900

    add: Problem10952 A+B - 5

---
+++ src/baekjoon/lv3/Problem10952.java
@@ -0,0 +1,33 @@
+package baekjoon.lv3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 이 문제는 입력 개수가 주어지지 않음 → for문 사용 불가 (정확하게는 무한 루프)
+ * "0 0"은 계산 대상이 아니라 입력 종료를 알리는 신호(sentinel)
+ */
+public class Problem10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer st =  new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a + b;
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            sb.append(sum).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
