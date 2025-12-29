acbd21a7681dac1f9c2a208ae03ff0a09668ec77
dideo233 <eltmvpdj11@gmail.com>
2025-12-30 08:39:52 +0900

    update: Problem1546 평균 코드 수정

--- src/baekjoon/lv4/Problem1546.java
+++ src/baekjoon/lv4/Problem1546.java
@@ -8,21 +8,23 @@ import java.util.StringTokenizer;
 public class Problem1546 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        float[] scores = new float[n];
-
-        float max = Integer.MIN_VALUE;
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int max = 0;
+        int[] scores = new int[n];
+
         for (int i = 0; i < n; i++) {
-            scores[i] = Float.parseFloat(st.nextToken());
-            if(scores[i] > max) max = scores[i];
+            scores[i] = Integer.parseInt(st.nextToken());
+            if (scores[i] > max) {
+                max = scores[i];
+            }
         }
 
-        float sum = 0;
+        double sum = 0.0;
         for (int i = 0; i < n; i++) {
-            scores[i] = (scores[i] / max) * 100;
-            sum += scores[i];
+            sum += (double) scores[i] / max * 100;
         }
 
         System.out.println(sum / n);
