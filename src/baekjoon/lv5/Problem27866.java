d883e05c94017ce70467bebe5bcb0772c3e83556
dideo233 <eltmvpdj11@gmail.com>
2025-12-30 08:49:34 +0900

    add: Problem27866 문자와 문자열

--- src/baekjoon/lv5/Problem27866.java
+++ src/baekjoon/lv5/Problem27866.java
@@ -1,4 +1,19 @@
 package baekjoon.lv5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * [문제 맥락]
+ * - 문자열 S와 정수 i가 주어질 때
+ * - 문자열 S의 i번째 문자를 출력하는 문제
+ */
 public class Problem27866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(str.charAt(n-1));
+    }
 }
