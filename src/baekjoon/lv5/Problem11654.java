389da6cbb686b39f802a8a476bdd028412221e32
dideo233 <eltmvpdj11@gmail.com>
2026-01-21 13:45:09 +0900

    add: Problem11654 아스키 코드

---
+++ src/baekjoon/lv5/Problem11654.java
@@ -0,0 +1,13 @@
+package baekjoon.lv5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] input= br.readLine().toCharArray();
+        System.out.println((int) input[0]);
+    }
+}
