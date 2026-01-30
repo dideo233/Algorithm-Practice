ea9bec49b92a129e5e75f7e91dfe923e6fd555b1
dideo233 <eltmvpdj11@gmail.com>
2026-01-30 23:32:15 +0900

    add: Problem25083 새싹 출력

---
+++ src/baekjoon/lv6/Problem25083.java
@@ -0,0 +1,17 @@
+package baekjoon.lv6;
+
+/**
+ * [문제 맥락]
+ * - 큰따옴표(")와 역슬래시(\) 등 예약 문자를 이스케이프 시퀸스로 처리하는 문제
+ * - java 15 이후부터는 Text Block(""") 문법으로 간단히 처리 가능
+ */
+public class Problem25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
