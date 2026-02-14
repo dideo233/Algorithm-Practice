49a8747941e2ac26e0f55ab4d2462e80fe98b8d5
dideo233 <eltmvpdj11@gmail.com>
2026-02-14 23:55:35 +0900

    update: Program2738 변수명 변경

--- src/baekjoon/lv7/Program2738.java
+++ src/baekjoon/lv7/Program2738.java
@@ -23,13 +23,13 @@ public class Program2738 {
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] result = new int[N][M];
+        int[][] processtion = new int[N][M];
 
         // 행렬 A 입력
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                result[i][j] = Integer.parseInt(st.nextToken());
+                processtion[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
@@ -37,7 +37,7 @@ public class Program2738 {
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                result[i][j] += Integer.parseInt(st.nextToken());
+                processtion[i][j] += Integer.parseInt(st.nextToken());
             }
         }
 
@@ -45,7 +45,7 @@ public class Program2738 {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                sb.append(result[i][j]).append(" ");
+                sb.append(processtion[i][j]).append(" ");
             }
             sb.append("\n");
         }
